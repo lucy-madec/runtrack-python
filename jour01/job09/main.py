5a8db61737786ea753d4cc82f7ad889e52398475
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2023-11-20 14:24:00 +0100

    Modification job09

--- jour01/job09/main.py
+++ jour01/job09/main.py
@@ -1,8 +1,10 @@
 nom = "Smartphone"
 PU = 229
-quantité = "100"
+quantité = 100
 print ("Nom du produit :", nom, "Prix Unitaire :", PU, "Quantité :", quantité)
 PU += PU * (10/100)
 print ('Veuillez indiquer le nombre de quantité souhaité :')
 nbquantité = int(input())
+quantité -=nbquantité
+print ('Il reste', quantité, 'Smartphones en stock.')
 print ('Prix après inflation :', PU)
