3a8295762334f7df0ac66d4c473b92e8d51bf0ae
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2023-11-22 13:57:14 +0100

    job10 terminé

---
+++ jour03/job10/main.py
@@ -0,0 +1,11 @@
+def nb_pair_impair(nombre):
+    if nombre % 2 == 0 and nombre > 0 and isinstance(nombre, int):
+        print("Nombre pair")    
+    elif nombre % 2 != 0 and nombre > 0 and isinstance(nombre, int):
+        print("Nombre impair")
+    else:
+        print("Ce nombre n'est pas un nombre entier ou positif.")
+
+nb_pair_impair(46)
+nb_pair_impair(45)
+nb_pair_impair(-13)
