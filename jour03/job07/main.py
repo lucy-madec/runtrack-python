61efacd38c74de4ac8c78c09aa16258fed5c284d
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2023-11-22 11:42:38 +0100

    job07 terminé

---
+++ jour03/job07/main.py
@@ -0,0 +1,17 @@
+def langage(langage):
+    if langage == "JavaScript":
+        return "tu es un développeur web"
+    elif langage == "python":
+        return "tu es un développeur IA"
+    elif langage == "java":
+        return "tu es un développeur logiciel"
+    elif langage == "reactjs":
+        return "tu es un developpeur mobile"
+    else :
+        return "un jour, je serai le meilleur développeur..."
+
+print (langage("JavaScript"))
+print (langage("python"))
+print (langage("java"))
+print (langage("reactjs"))
+print (langage("html"))
