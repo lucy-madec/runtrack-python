de06d9a3aa61f1c88e80ca76001e926ae5ed33a2
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2023-11-23 15:01:17 +0100

    job09 terminé

---
+++ jour04/job09/main.py
@@ -0,0 +1,7 @@
+L = [8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
+
+valeur_maximale = max(L)
+valeur_minimale = min(L)
+
+print("La valeur max est :", valeur_maximale)
+print("La valeur min est :", valeur_minimale)
